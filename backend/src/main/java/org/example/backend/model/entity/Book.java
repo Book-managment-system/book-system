57c2b197b3f6935df219e7268be17e9a10aee184
Abdelrahman Mostafa Kamel <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2025-12-26 19:32:56 +0200

    feat: add book domain model

---
+++ backend/src/main/java/org/example/backend/model/entity/Book.java
@@ -0,0 +1,92 @@
+package org.example.backend.model.entity;
+
+import org.example.backend.model.enums.Category;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private Integer publisherId;
+    private Integer publicationYear;
+    private Float sellingPrice;
+    private Category category;
+    private Integer numberOfBooks;
+    private Integer threshold;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, Integer publisherId, Integer publicationYear, Float sellingPrice, Category category, Integer numberOfBooks, Integer threshold) {
+        this.isbn = isbn;
+        this.title = title;
+        this.publisherId = publisherId;
+        this.publicationYear = publicationYear;
+        this.sellingPrice = sellingPrice;
+        this.category = category;
+        this.numberOfBooks = numberOfBooks;
+        this.threshold = threshold;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Float getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public Integer getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+    public Integer getPublisherId() {
+        return publisherId;
+    }
+
+    public Integer getThreshold() {
+        return threshold;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setNumberOfBooks(Integer numberOfBooks) {
+        this.numberOfBooks = numberOfBooks;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setPublisherId(Integer publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public void setSellingPrice(Float sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public void setThreshold(Integer threshold) {
+        this.threshold = threshold;
+    }
+}
